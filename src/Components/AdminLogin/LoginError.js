fe578a29e2bcea6e873653baeaccaed514f28202
Talha-Jubaer-Prantor <talhajubaer3020@gmail.com>
2023-01-04 23:57:03 +0600

    adding backend

--- src/Components/AdminLogin/LoginError.js
+++ src/Components/AdminLogin/LoginError.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import "./LoginError.css"
 
 const LoginError = () => {
     return (
-        <div>
+        <div style={{textAlign:"center",marginTop:"50px"}}>
             <Link to={"/adminlogin"} className='btn btn-success'>Try Again</Link>
             
         </div>
