9617163fa5204c36e3c096a66718011d4d1d790d
Talha Jubaer Prantor <85910000+Talha-Jubaer-Prantor@users.noreply.github.com>
2023-01-05 23:05:33 +0600

    Update AdminLogin.js
    Bug

--- src/Components/AdminLogin/AdminLogin.js
+++ src/Components/AdminLogin/AdminLogin.js
@@ -21,7 +21,7 @@ const AdminLogin = () => {
       password: password,
     };
     setIsLoading(true);
-    await fetch("https://rm-fashion-backend.vercel.app0/loginuser", {
+    await fetch("https://rm-fashion-backend.vercel.app/loginuser", {
       headers: {
         "Content-Type": "application/json",
       },
